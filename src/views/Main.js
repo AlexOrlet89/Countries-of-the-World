5913b7273116e98062210a11b7ec24bce276f77d
Alex Orlet <AlexOrlet89@gmail.com>
2022-03-15 16:37:16 -0700

    return statement renders flag data from countries table

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,16 +1,25 @@
-import { useEffect, useState } from 'react';
-import { getFlags } from '../services/flags';
+import { useEffect, useState } from 'react'; //two functions imported from react, which we installed using npm i
+import { getFlags } from '../services/flags'; //imports data from the countries table
 
 export default function Main() {
   const [flags, setFlags] = useState([]);
 
   useEffect(() => {
-    const fetchBlogs = async () => {
+    const fetchFlags = async () => {
       const resp = await getFlags();
+      console.log(resp);
       setFlags(resp);
     };
     console.log('use effect ran');
-  });
+    console.log({ flags });
+    fetchFlags();
+  }, []);
 
-  return <div className="App"></div>;
+  return (
+    <main>
+      {flags.map((flag) => (
+        <p key={flag.iso2}>{flag.name}</p>
+      ))}
+    </main>
+  );
 }
